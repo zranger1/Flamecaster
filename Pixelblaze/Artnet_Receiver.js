9eb1c832b5dc6105b5642fcdca1dafeb496d39b3
ZRanger1 <35553094+zranger1@users.noreply.github.com>
2024-02-24 17:52:26 -0700

    Update Pixelblaze patterns

--- Pixelblaze/Artnet_Receiver.js
+++ Pixelblaze/Artnet_Receiver.js
@@ -3,5 +3,5 @@ export var pixels = array(pixelCount)
 export function render(index) {
   var p = pixels[index]
   r = (p >> 8) & 0xff; g = p & 0xff; b = (p * 256 + .5) & 0xff
-  rgb(r, g, b)
+  rgb(r /255, g/255, b/255)
 }
